2c1570ebfa014fbb4f8a635ecb52e41220a323c1
Josue <Josue_0096@live.it>
2022-09-26 22:58:40 +0200

    esercizio 4

---
+++ ese4.js
@@ -0,0 +1,10 @@
+/*
+Scrivi un programma che dato un numero 
+conti da quante cifre è formato.
+*/
+
+let numero = 237086412;
+numero = String(numero);
+let cifre = numero.length;
+
+console.log("numero di cifre: " + cifre);
